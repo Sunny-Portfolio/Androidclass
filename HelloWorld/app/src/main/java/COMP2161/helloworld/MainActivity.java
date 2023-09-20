270dd4d828de7586f6a4e2442686b72270a8b5c4
Sunny <107274023+Sunny-Portfolio@users.noreply.github.com>
2023-09-20 10:52:12 -0700

    Add existing files to github

---
+++ HelloWorld/app/src/main/java/COMP2161/helloworld/MainActivity.java
@@ -0,0 +1,14 @@
+package COMP2161.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
