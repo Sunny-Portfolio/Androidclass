d747bcb7e8eaf96fd16db1135e6e3e2eac19c683
Sunny <107274023+Sunny-Portfolio@users.noreply.github.com>
2023-10-29 14:15:13 -0400

    Implemented ACTION_TIME_TICK broadcast receiver to update passcode

--- SecurityTokenApp/app/src/main/java/com/example/securitytokenapp/MainActivity.java
+++ SecurityTokenApp/app/src/main/java/com/example/securitytokenapp/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Calendar;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
@@ -23,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
     private TextView textview_countdown;
     private Button button_verify;
     private CountDownTimer timer;
+    private BroadcastReceiver minuteChangedReceiver;
+    // Broadcast intent action
+    public static final String PASSCODE_UPDATE_ACTION = "com.example.securitytokenapp.passcode_update";
+
 
 
 
@@ -38,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
         textview_countdown = findViewById(R.id.textView_countdown);
         button_verify = findViewById(R.id.btn_verify);
 
+
+
         /**
          * On Click Listener for the verification button
          */
@@ -50,8 +59,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        passcode = renew_passcode();
+        textview_passcode.setText(String.valueOf(passcode));
 
-        renew_passcode();
+
+
+        /**
+         * This broadcast receiver receive ACTION_TIME_TICK which is sent when the minutes changesd
+         * After that, the passcode is changed and synced
+         */
+        minuteChangedReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (Intent.ACTION_TIME_TICK.equals(intent.getAction())) {
+                    // Update the passcode when the minutes change
+                    int passcode = renew_passcode();
+                    textview_passcode.setText(String.valueOf(passcode));
+
+                    // Start the 60 seconds timer
+                    timer.start();
+
+                    // Broadcast the passcode to Activity2
+                    Intent passcodeIntent = new Intent(PASSCODE_UPDATE_ACTION);
+                    passcodeIntent.putExtra("passcode", passcode);
+                    sendBroadcast(passcodeIntent);
+                }
+            }
+        };
+
+        // Register the broadcast receiver for ACTION_TIME_TICK
+        registerReceiver(minuteChangedReceiver, new IntentFilter(Intent.ACTION_TIME_TICK));
+
+
 
         /**
          * Setup the timer and start counting
@@ -66,14 +105,40 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-                renew_passcode();
-                start();
+//                int passcode = renew_passcode();
+//                textview_passcode.setText(String.valueOf(passcode));
+//
+//                // TODO: 10/28/23 send braoadcast this way maybe? check gpt
+//                // maybe use ACTION_TIME_TICK instead of 60 secs countdown
+//                // because it suppose to sync with system instead my own countdown
+//                // Broadcast an intent
+//                Intent intent = new Intent();
+//                intent.setAction("")
+//                intent.putExtra("passcode", passcode);
+//                sendBroadcast(intent);
+//
+//                LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(context);
+//
+//
+//                start();
             }
-        }.start();
+        };
 
 
         Log.d("TAG", "onCreate: minutes is = " + current_minute);
 
+    }
+
+
+
+    /**
+     * Unregister the broadcast receiver when MainActivity is destroyed
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        unregisterReceiver(minuteChangedReceiver);
     }
 
     /**
@@ -81,13 +146,12 @@ public class MainActivity extends AppCompatActivity {
      * Using the Java Calendar class instead of System.currentTimeMillis()
      * Both should work the same way
      */
-    public void renew_passcode () {
+    public int renew_passcode () {
         cal = Calendar.getInstance();
 
         current_minute = cal.get(Calendar.MINUTE);
-        passcode = current_minute * 1245 + 10000;
+        return current_minute * 1245 + 10000;
 
-        textview_passcode.setText(String.valueOf(passcode));
     }
 
 
