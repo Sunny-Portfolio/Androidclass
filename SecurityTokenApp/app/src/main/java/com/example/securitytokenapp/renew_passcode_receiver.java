204ce2fc36ba1769c66c4752344645aef9094406
Sunny <107274023+Sunny-Portfolio@users.noreply.github.com>
2023-10-28 20:33:18 -0400

    Created base for broadcast receiver

---
+++ SecurityTokenApp/app/src/main/java/com/example/securitytokenapp/renew_passcode_receiver.java
@@ -0,0 +1,12 @@
+package com.example.securitytokenapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class renew_passcode_receiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
