c29c4cd59faa38b11694975bb220a040205fa8e5
Sunny <107274023+Sunny-Portfolio@users.noreply.github.com>
2023-11-19 18:16:00 -0500

    Implemented back to menu

--- TicTacToe/app/src/main/java/com/example/tictactoe/GameWonDialog.java
+++ TicTacToe/app/src/main/java/com/example/tictactoe/GameWonDialog.java
@@ -2,6 +2,7 @@ package com.example.tictactoe;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -50,7 +51,10 @@ public class GameWonDialog extends Dialog {
         mainMenuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                mainActivity.returnToMainMenu();
+                // Start a new intent and clear activity stack
+                Intent mainActivityIntent = new Intent(getContext(), MainActivity.class);
+                mainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                getContext().startActivity(mainActivityIntent);
                 dismiss();
             }
         });
